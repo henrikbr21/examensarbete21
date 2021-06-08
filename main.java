d3a7b8fba11b8566efee88726d12e36558263b3d
henrikbr21 <84152025+henrikbr21@users.noreply.github.com>
2021-06-08 12:40:36 +0200

    Attempted implementation of bishop move generation.

---
+++ main.java
@@ -0,0 +1,17 @@
+package engine;
+
+public class main {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		Engine engine = new Engine("WHITE", board);
+		
+		long time = System.currentTimeMillis();
+		for(int i = 0; i < 1; i++) {
+			engine.generateMoves("WHITE");
+		}
+		System.out.println(System.currentTimeMillis()-time);
+		
+	}
+
+}
