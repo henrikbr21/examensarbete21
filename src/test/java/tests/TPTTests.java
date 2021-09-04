0e11c0e846ca85554a635396901b4971b9e0fb63
Henrik Brange <henrikbr@live.com>
2021-09-04 23:27:26 +0200

    Added more tests to check proper functionality of TPT
    Read header

--- src/test/java/tests/TPTTests.java
+++ src/test/java/tests/TPTTests.java
@@ -7,6 +7,7 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Comparator;
 
@@ -47,6 +48,77 @@ public class TPTTests{
 
         System.out.println("RefCount: " + tpt.get(10).refCount);
         assertTrue(tpt.get(10).refCount == 2);
+    }
+
+    @Test
+    public void testEntryValidity(){
+        Board board = new Board();
+        TPT tpt = new TPT(300000);
+        Engine engine = new Engine("WHITE", board, false, tpt);
+        ArrayList<Move> pv = new ArrayList<>();
+        engine.alphaBetaMax(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv, -1, 0L);
+        assertTrue(pv.size()==4);
+
+        char[][] testBoard = {
+                {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'P', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+        };
+        Board board2 = new Board(testBoard);
+        long hash = tpt.hash(board2);
+        assertTrue(tpt.containsKey(hash));
+        double score1 = tpt.get(hash).score;
+        assertTrue(tpt.get(hash).depth == 3);
+
+        TPT tpt2 = new TPT(300000);
+        Engine engine2 = new Engine("WHITE", board, true, tpt2);
+        engine2.alphaBetaMax(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv, -1, 0L);
+        assertTrue(tpt2.containsKey(hash));
+        assertTrue(tpt2.get(hash).score == score1);
+    }
+
+    @Test
+    public void testEntryValidity2(){
+        Board board = new Board();
+        TPT tpt = new TPT(300000);
+        Engine engine = new Engine("WHITE", board, false, tpt);
+        ArrayList<Move> pv = new ArrayList<>();
+        ArrayList<Move> pv2 = new ArrayList<>();
+
+        engine.alphaBetaMin(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv, -1, 0L);
+        engine.alphaBetaMax(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv, -1, 0L);
+
+        engine.alphaBetaMax(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv2, -1, 0L);
+
+        for(int i = 0; i < pv.size(); i++){
+            assertTrue(pv.get(i).from == pv2.get(i).from);
+            assertTrue(pv.get(i).to == pv2.get(i).to);
+        }
+    }
+
+    @Test
+    public void testEntryValidity3(){
+        Board board = new Board();
+        TPT tpt = new TPT(300000);
+        Engine engine = new Engine("WHITE", board, false, tpt);
+        ArrayList<Move> pv = new ArrayList<>();
+        ArrayList<Move> pv2 = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            engine.alphaBetaMax(board, i, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv, -1, 0L);
+        }
+
+        engine.alphaBetaMax(board, 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, pv2, -1, 0L);
+
+        for(int i = 0; i < pv.size(); i++){
+            assertTrue(pv.get(i).from == pv2.get(i).from);
+            assertTrue(pv.get(i).to == pv2.get(i).to);
+        }
     }
 
 }
